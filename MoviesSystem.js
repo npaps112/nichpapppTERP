86feb13c3e9ff25856ba4f3bce975baa391b99df
Nicholas Pappas <n.pappas112@gmail.com>
2025-12-17 15:52:47 -0500

    new commit

--- MoviesSystem.js
+++ MoviesSystem.js
@@ -9,11 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 
-/*
+
 require("dotenv").config({
    path: path.resolve(__dirname, "credentialsDontPost/.env"),
 });
-*/
 
 const databaseName = "CMSC335DB";
 const collectionName = "moviesCollection";
